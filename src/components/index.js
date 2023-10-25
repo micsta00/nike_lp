3aa852a1556ef995f04959b06e44db2ee91ea480
micsta00 <110664760+micsta00@users.noreply.github.com>
2023-10-25 13:15:28 +0200

    Add sections and footer

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import Button from "./Button"
+import Nav from "./Nav"
+import PopularProductCard from "./PopularProductCard"
+import ShoeCard from "./ShoeCard"
+import ServiceCard from "./ServiceCard"
+import ReviewCard from "./ReviewCard"
+
+export {
+    Button,
+    Nav,
+    PopularProductCard,
+    ShoeCard,
+    ServiceCard,
+    ReviewCard,
+}
